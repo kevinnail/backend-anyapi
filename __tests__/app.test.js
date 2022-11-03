4d9464b7e64f7a317ac854e5e41726ba208bc386
kevinnail <fumalicious@gmail.com>
2022-11-03 11:28:19 -0700

    missed added files on last commit, data files created for both sets of data

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,23 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const {songs} = require('../')
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  // it('example test - delete me!', () => {
+  //   expect(1).toEqual(1);
+  // });
+
+  it('/songs should return a list of songs', async()=>{
+    const res = await request(app).get('/songs');
+    const expected = 
   });
+
   afterAll(() => {
     pool.end();
   });
