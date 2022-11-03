37670141461b3f8617f198663552c1768e31425c
kevinnail <fumalicious@gmail.com>
2022-11-03 11:41:53 -0700

    first test written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,19 +3,26 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-const {songs} = require('../')
+const { songs } = require('../lib/songs-data.js');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  // it('example test - delete me!', () => {
-  //   expect(1).toEqual(1);
-  // });
 
-  it('/songs should return a list of songs', async()=>{
+  it('/songs should return a list of songs', async () => {
     const res = await request(app).get('/songs');
-    const expected = 
+    const expected = songs.map((song) => {
+      return {
+        id: song.id,
+        name: song.name,
+        band: song.band,
+        album: song.album,
+        year: song.year,
+        length: song.length,
+      };
+    });
+    expect(res.body).toEqual(expected);
   });
 
   afterAll(() => {
